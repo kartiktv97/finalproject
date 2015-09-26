efa6f192d1b74286a8487088a9262c508fbb4142
kartiktv97 <kartiktv97@gmail.com>
2015-09-26 16:45:19 -0400

    partially completed design

--- finalproject.rkt
+++ finalproject.rkt
@@ -3,9 +3,6 @@
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname finalproject) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
 ;;Final project animation
 
-;;an animation-window is a list of commands
-;;  -(make-animation-window list[cmd])
-
 ;;a velocity is
 ;;  -(make-velocity number number)
 (define-struct velocity (x y))
@@ -14,14 +11,34 @@
 ;;  -(make-posn number number)
 
 ;;a figure is either
-;;  -(make-circle (posn velocity string number string))
-;;  -(make-rectangle (posn velocity number number string string))
-(define circle (posn velocity radius type color))
-(define rectangle (posn velocity width height type color))
+;;  -(make-circle (string posn velocity string number string))
+;;  -(make-rectangle (string posn velocity number number string string))
+(define-struct circle (name posn velocity radius type color))
+(define-struct rectangle (name posn velocity width height type color))
+
+;;a stop-if is:
+;;  -(make-stop-if (hit))
+;; where hit is a list of strings containing these: 
+;;  -"right-edge"
+;;  -"left-edge"
+;;  -"top-edge"
+;;  -"bottom-edge"
+;; OR
+;;  -name of figure (string)
+(define-struct stop-if (hit))
 
 ;;a command is either:
+;;  -(make-displayworld (world))
 ;;  -(make-displayfigure (figure))
-(define displayfigure (figure))
+;;  -stop-if
+(define-struct displayfigure (figure))
+(define-struct displayworld (world))
+
+;;an animation is a list of commands
+;;  -(make-animation list[cmd])
+(define-struct animation (cmds))
+
+
 
 
 
