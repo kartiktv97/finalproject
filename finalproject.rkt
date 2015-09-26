c06bd26f443d236f1e1b26bba6f50e4fec83abe3
kartiktv97 <kartiktv97@gmail.com>
2015-09-26 15:56:00 -0400

    Added some comments

--- finalproject.rkt
+++ finalproject.rkt
@@ -1,3 +1,15 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname finalproject) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #t #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+;;Final project animation
+
+;;an animation-window is a list of commands
+
+;;a command is either:
+;;  -(make-displayfigure (figure))
+
+
+;;a figure is either
+;;  -(make-acircle (circle))
+;;  -(make-arectangle (rectangle))
+
